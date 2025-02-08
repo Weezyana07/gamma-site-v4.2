b319e94a05c38c761871ffeba1592bb790abdb29
weezyana07 <owills_bmer@outlook.com>
2025-02-08 12:59:32 +0100

    updated commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,116 +1,5 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-// import Industries from "../components/Industries";
-// import Services from "../components/Services";
-// import Projects from "../components/Projects";
-// import Testimonials from "../components/Testimonials";
-// import Contact from "../components/Contact";
-// import Footer from "../components/Footer";
-// import "./Home.css";
 
-// const Home = () => {
-//   return (
-//     <div>
-//       <header className="home-header">
-//         <div className="top-image"></div>
-//         <div className="mid-text">
-//           <p>
-//             At <span>GAMMA SPECTRE</span>, we specialize in providing
-//             comprehensive consultancy, information technology, and general
-//             solutions tailored to meet the diverse needs of our clients. With a
-//             deep understanding of industry trends and technological
-//             advancements, our team of experts collaborates closely with
-//             businesses to unlock their full potential and drive sustainable
-//             growth.
-//           </p>
-//         </div>
-//         <div className="bottom-image"></div>
-//         <div  className="container">
-//         <div className="wrapper">
-//           <div className="nav-wrapper">
-//             <div className="logo">
-//               <a href="#">
-//                 <img src="images/logo-home.png" alt="Manage" />
-//               </a>
-//             </div>
-//             <button
-//               className="mobile-nav-toggle"
-//               aria-controls="primary-navigation"
-//               aria-expanded="false"
-//             >
-//               <img
-//                 className="icon-hamburger"
-//                 src="images/icon-hamburger.png"
-//                 alt="menu"
-//               />
-//               <span className="visually-hidden">Menu</span>
-//             </button>
-//             <nav className="primary-navigation">
-//               <ul role="list" className="nav-list" id="primary-navigation">
-//                 <li>
-//                   <NavLink
-//                     exact
-//                     to="/"
-//                     className={({ isActive }) =>
-//                       isActive ? "nav-link active-link" : "nav-link"
-//                     }
-//                   >
-//                     Home
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink
-//                     to="/about"
-//                     className={({ isActive }) =>
-//                       isActive ? "nav-link active-link" : "nav-link"
-//                     }
-//                   >
-//                     About
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink
-//                     to="/services"
-//                     className={({ isActive }) =>
-//                       isActive ? "nav-link active-link" : "nav-link"
-//                     }
-//                   >
-//                     Services
-//                   </NavLink>
-//                 </li>
-//                 <li>
-//                   <NavLink
-//                     to="/contact"
-//                     className={({ isActive }) =>
-//                       isActive ? "nav-link active-link" : "nav-link"
-//                     }
-//                   >
-//                     Contact
-//                   </NavLink>
-//                 </li>
-//               </ul>
-//             </nav>
-//           </div>
-//         </div>
-//         <div className="welcome">
-//           <h1 id="h1">Unlocking Potential:</h1>
-//           <h2 id="h2">Transforming Businesses with Technology and Insight.</h2>
-//         </div>
-//         </div>
-//       </header>
-//       <Services />
-//       <Industries />
-//       <Projects />
-//       <Testimonials />
-//       <Contact />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Industries from "../components/Industries";
 import Services from "../components/Services";
@@ -121,7 +10,20 @@ import Footer from "../components/Footer";
 import "./Home.css";
 
 const Home = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isNavScrolled, setIsNavScrolled] = useState(false);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsNavScrolled(true);
+      } else {
+        setIsNavScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div>
@@ -139,80 +41,72 @@ const Home = () => {
           </p>
         </div>
         <div className="bottom-image"></div>
-        <div className="container">
-          <div className="wrapper">
-            <div className="nav-wrapper">
-              <div className="logo">
-                <a href="#">
-                  <img src="images/logo-home.png" alt="Manage" />
-                </a>
-              </div>
-              <button
-                className="mobile-nav-toggle"
-                onClick={() => setMenuOpen(!menuOpen)}
-                aria-controls="primary-navigation"
-                aria-expanded={menuOpen}
-              >
-                <img
-                  className="icon-hamburger"
-                  src="images/icon-hamburger.png"
-                  alt="menu"
-                />
-              </button>
-              <nav
-                className={`primary-navigation ${menuOpen ? "open" : ""}`}
-                id="primary-navigation"
-              >
-                <ul className="nav-list">
-                  <li>
-                    <NavLink
-                      to="/"
-                      className={({ isActive }) =>
-                        isActive ? "nav-link active-link" : "nav-link"
-                      }
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/about"
-                      className={({ isActive }) =>
-                        isActive ? "nav-link active-link" : "nav-link"
-                      }
-                    >
-                      About
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/services"
-                      className={({ isActive }) =>
-                        isActive ? "nav-link active-link" : "nav-link"
-                      }
-                    >
-                      Services
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/contact"
-                      className={({ isActive }) =>
-                        isActive ? "nav-link active-link" : "nav-link"
-                      }
-                    >
-                      Contact
-                    </NavLink>
-                  </li>
+
+           {/* Navigation */}
+        <div className={`nav-wrapper ${isNavScrolled ? "sticky-nav" : ""}`}>
+          <div className="logo">
+            <a href="#">
+              <img
+                src="images/logo-home.png"
+                alt="Logo"
+                className={isNavScrolled ? "logo-small" : ""}
+              />
+            </a>
+          </div>
+          <button
+            className="mobile-nav-toggle"
+            onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+            aria-controls="primary-navigation"
+            aria-expanded={isMobileNavOpen}
+          >
+            <img
+              className="icon-hamburger"
+              src="images/icon-hamburger.png"
+              alt="Menu"
+            />
+          </button>
+          <nav
+            className={`primary-navigation ${
+              isMobileNavOpen ? "open" : "closed"
+            }`}
+          >
+            <ul className="nav-list">
+              <li>
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className="nav-link">
+                  About
+                </NavLink>
+              </li>
+              <li className="dropdown-container">
+                <NavLink to="/services" className="nav-link">
+                  Services
+                </NavLink>
+                <ul className="dropdown">
+                  <li>Consultancy</li>
+                  <li>Information Technology</li>
+                  <li>Manufacturing</li>
+                  <li>Construction</li>
+                  <li>General Contracts</li>
                 </ul>
-              </nav>
-            </div>
-          </div>
-          <div className="welcome">
-            <h1>Unlocking Potential:</h1>
-            <h2>Transforming Businesses with Technology and Insight.</h2>
-          </div>
+              </li>
+              <li>
+                <NavLink to="/contact" className="nav-link">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
+          <div className="welcome">
+          <h1 className="hero-title">Unlocking Potential:</h1>
+            <h2 className="hero-subtitle">
+              Transforming Businesses with Technology and Insight.
+            </h2>
+            </div>
       </header>
       <Services />
       <Industries />
