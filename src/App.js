87809ebd847a29bd3d741feca84fd33cc9f69c65
weezyana07 <owills_bmer@outlook.com>
2025-02-17 10:26:23 +0100

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import ServicePage from "./pages/ServicePage";
 import AboutPage from "./pages/AboutPage";
@@ -8,17 +9,8 @@ import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Navbar />
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />} />
-    //     <Route path="/services" element={<ServicePage />} />
-    //     <Route path="/about" element={<AboutPage />} />
-    //     <Route path="/contact" element={<ContactPage />} />
-    //   </Routes>
-    // </BrowserRouter>
     <Router>
-      {/* <Header /> */}
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicePage />} />
